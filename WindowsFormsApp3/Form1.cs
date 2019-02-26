ff43f15dc028e8e8e68617dd8d426251ba5a52ce
rc0035391 <rc0035391@CRO-24-02>
2019-02-26 09:23:37 +0000

    Add project files.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double pi = 3.141592653589793;
+            double c = 299792458;
+            richTextBox1.AppendText(c.ToString() + "\n");
+            richTextBox1.AppendText(c.ToString("N") + "\n");
+            richTextBox1.AppendText(c.ToString("N3") + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+            richTextBox1.AppendText(c.ToString("0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.00E00") + "\n");
+            richTextBox1.AppendText(pi.ToString() + "\n");
+            richTextBox1.AppendText(pi.ToString("N") + "\n");
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.00E00") + "\n");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
